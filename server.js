ec55f1be6022492e1f1d8956904801613891f20a
drfrostongithub <eternal.putra@gmail.com>
2025-01-11 18:18:29 +0700

    refactor some code

--- server.js
+++ server.js
@@ -7,16 +7,24 @@
 
 require(`dotenv`).config();
 
-const express = require(`express`);
+const express = require("express");
+const cors = require("cors");
+const errorHandler = require("./middlewares/errorHandler");
+const routes = require("./routes");
 
 const app = express();
-const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
-//use cors here
+// Middleware
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Routes
+app.use(routes);
+
+// Error Handler
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("Crypto Wallet Backend is running!");
