da8aa9121bcf793b41e46fd4b6803d0ab27ddf12
drfrostongithub <eternal.putra@gmail.com>
2025-01-12 15:08:05 +0700

    adding wallet routes.js

---
+++ routes/walletRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const walletController = require("../controllers/walletController");
+const authentication = require("../middlewares/authentication");
+
+// Apply authentication to wallet routes
+router.use(authentication);
+
+// Wallet operations
+router.post("/create", walletController.createWallet);
+router.get("/balance", walletController.getBalance);
+router.post("/deposit", walletController.deposit);
+router.post("/transfer", walletController.transfer);
+router.get("/transactions/top/:n", walletController.listTopTransactions);
+router.get("/transactions/users/top", walletController.listTopUsers);
+
+module.exports = router;
