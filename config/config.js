3b6e8fbb39ec464571489bc277fbcaa8ecb4bf32
drfrostongithub <eternal.putra@gmail.com>
2025-01-13 01:30:45 +0700

    local add to config

--- config/config.js
+++ config/config.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 
 module.exports = {
+  local: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    url: process.env.DATABASE_URL,
+  },
   development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
