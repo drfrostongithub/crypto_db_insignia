04a4c5123284820a38663cb79c6692eaeb3cdf89
drfrostongithub <eternal.putra@gmail.com>
2025-01-13 00:31:00 +0700

    add production for deployment

--- config/config.js
+++ config/config.js
@@ -17,4 +17,12 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
     use_env_variable: "TEST_DATABASE_URL",
   },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    use_env_variable: "PRODUCTION_DATABASE_URL",
+  },
 };
