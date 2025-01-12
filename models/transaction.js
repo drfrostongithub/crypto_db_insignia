9c77e1990ee828435f62cbdb0b499ebe1f150502
drfrostongithub <eternal.putra@gmail.com>
2025-01-12 16:40:16 +0700

    migrations and model transactions

---
+++ models/transaction.js
@@ -0,0 +1,66 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Transaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Transaction.belongsTo(models.User, {
+        as: "Sender",
+        foreignKey: "senderId",
+      });
+      Transaction.belongsTo(models.User, {
+        as: "Recipient",
+        foreignKey: "recipientId",
+      });
+      Transaction.belongsTo(models.Wallet, {
+        as: "SenderWallet",
+        foreignKey: "senderId",
+      });
+      Transaction.belongsTo(models.Wallet, {
+        as: "RecipientWallet",
+        foreignKey: "recipientId",
+      });
+    }
+  }
+  Transaction.init(
+    {
+      senderId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Sender ID is required" },
+          isInt: { msg: "Sender ID must be an integer" },
+        },
+      },
+      recipientId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Recipient ID is required" },
+          isInt: { msg: "Recipient ID must be an integer" },
+        },
+      },
+      amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          notNull: { msg: "Transaction amount is required" },
+          isFloat: { msg: "Amount must be a valid number" },
+          min: {
+            args: [0.01],
+            msg: "Transaction amount must be greater than zero",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Transaction",
+    }
+  );
+  return Transaction;
+};
