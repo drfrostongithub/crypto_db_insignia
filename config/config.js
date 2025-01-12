32c229ddac8132c6e5b33a916b964fec085fca7f
drfrostongithub <eternal.putra@gmail.com>
2025-01-12 23:53:49 +0700

    env debacle fixed

--- config/config.js
+++ config/config.js
@@ -7,5 +7,14 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
+    use_env_variable: "DATABASE_URL",
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    use_env_variable: "TEST_DATABASE_URL",
   },
 };
