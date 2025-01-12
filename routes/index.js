9c7777cd1987b7bd3ed01daddce029d3da9a4b5d
drfrostongithub <eternal.putra@gmail.com>
2025-01-13 02:09:33 +0700

    another debacle for router dev

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const authentication = require("../middlewares/authentication");
 const userController = require("../controllers/userController");
 const Controller = require(`../controllers/controller`);
 const walletRoutes = require("./walletRoutes");
 
-// router.get(`/`, Controller.home);
+router.get(`/`, Controller.home);
 
 // Public routes
 router.post("/users/register", userController.register);
 router.post("/users/login", userController.login);
 
 // Protected routes
+router.use(authentication);
 router.use("/wallets", walletRoutes);
 
 module.exports = router;
--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const walletController = require("../controllers/walletController");
-const authentication = require("../middlewares/authentication");
 
 // Apply authentication to wallet routes
-router.use(authentication);
+
 // Wallet operations
 // router.post("/create", walletController.createWallet);
 router.get("/balance", walletController.getBalance);
