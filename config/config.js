77127125134f6465beefde5fab401e9ef7404bd8
drfrostongithub <eternal.putra@gmail.com>
2025-01-14 00:30:13 +0700

    config fix

--- config/config.js
+++ config/config.js
@@ -23,15 +23,15 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: "127.0.0.1",
     dialect: "postgres",
-    url: process.env.TEST_DATABASE_URL,
+    url: "//postgres:putra021825@localhost:5432/crypto_insignia_db_test",
   },
   production: {
     username: "postgres",
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: "crypto_insignia_db",
     host: "127.0.0.1",
     dialect: "postgres",
-    url: process.env.PRODUCTION_DATABASE_URL,
+    url: "//postgres:putra021825@localhost:5432/crypto_insignia_db_prod",
     use_env_variable: "JAWSDB_URL",
   },
 };
