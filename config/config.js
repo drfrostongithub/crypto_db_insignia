2af390b879d18dd14bc359657a2f507a4e8d5434
drfrostongithub <eternal.putra@gmail.com>
2025-01-14 01:52:12 +0700

    no url

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    url: process.env.DATABASE_URL,
+    // url: process.env.DATABASE_URL,
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -15,7 +15,7 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    url: process.env.TEST_DATABASE_URL,
+    // url: process.env.TEST_DATABASE_URL,
   },
   production: {
     username: process.env.DB_USERNAME,
@@ -23,7 +23,7 @@ module.exports = {
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    url: process.env.PRODUCTION_DATABASE_URL,
+    // url: process.env.PRODUCTION_DATABASE_URL,
     use_env_variable: "JAWSDB_URL",
   },
 };
