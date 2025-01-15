dddc379eeeb4ea48badb9e40976f7048620b6d1b
drfrostongithub <eternal.putra@gmail.com>
2025-01-15 20:33:14 +0700

    fix config again

--- config/config.js
+++ config/config.js
@@ -17,23 +17,23 @@ module.exports = {
     dialect: "postgres",
     // url: process.env.TEST_DATABASE_URL,
   },
-  // production: {
-  //   username: process.env.DB_USERNAME,
-  //   password: process.env.DB_PASSWORD,
-  //   database: process.env.DB_DATABASE,
-  //   host: process.env.DB_HOST,
-  //   dialect: "postgres",
-  //   // url: process.env.PRODUCTION_DATABASE_URL,
-  //   // use_env_variable: "JAWSDB_URL",
-  // },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Necessary for some cloud providers
-      },
-    },
+    url: process.env.PRODUCTION_DATABASE_URL,
+    // use_env_variable: "JAWSDB_URL",
   },
+  // production: {
+  //   use_env_variable: process.env.DATABASE_URL,
+  //   dialect: "postgres",
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false, // Necessary for some cloud providers
+  //     },
+  //   },
+  // },
 };
