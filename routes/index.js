f4ec55831e87bf7de9747c70f88c1c23af77641f
drfrostongithub <eternal.putra@gmail.com>
2025-01-16 03:44:16 +0700

    offing auth

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 
 // Protected routes
-router.use(authentication);
+// router.use(authentication);
 router.use("/wallets", walletRoutes);
 
 module.exports = router;
