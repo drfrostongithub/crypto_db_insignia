c701dc04102e54ab967b83bf65d68c638fb47754
drfrostongithub <eternal.putra@gmail.com>
2025-04-20 00:26:21 +0700

    commit to check again

--- api/index.js
+++ api/index.js
@@ -1,18 +1,57 @@
+// if (process.env.NODE_ENV !== "production") {
+//   require(`dotenv`).config();
+//   // config();
+// }
+
+require(`dotenv`).config();
+
 const express = require("express");
-const router = express.Router();
-const authentication = require("../middlewares/authentication");
-const UserController = require("../controllers/userController");
-const Controller = require(`../controllers/controller`);
-const walletRoutes = require("../routes/walletRoutes");
+const cors = require("cors");
+const errorHandler = require("../middlewares/errorHandler");
+const routes = require("../routes");
+const config = require("../config/config");
+const Sequelize = require("sequelize");
 
-router.get(`/`, Controller.home);
-router.get(`/check`, Controller.check);
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-// Public routes
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
+console.log(config[process.env.NODE_ENV]);
+if (process.env.NODE_ENV && config[process.env.NODE_ENV]) {
+  sequelize = new Sequelize(config[process.env.NODE_ENV]);
+} else {
+  const dbName = process.env.DB_DATABASE;
+  const dbUser = process.env.DB_USERNAME;
+  const dbPassword = process.env.DB_PASSWORD;
+  const dbConfig = { dialect: "postgres" };
 
-// Protected routes
-router.use("/wallets", authentication, walletRoutes);
+  if (!dbName || !dbUser || !dbPassword) {
+    throw new Error(
+      "Database configuration is missing in environment variables"
+    );
+  }
+  sequelize = new Sequelize(dbName, dbUser, dbPassword, dbConfig);
+}
 
-module.exports = router;
+// Middleware
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Routes
+app.use(routes);
+
+// Error Handler
+app.use(errorHandler);
+
+// app.get("/", (req, res) => {
+//   res.send("Crypto Wallet Backend is running!");
+// });
+
+// Start the server only in production or non-test environments
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,6 @@
 class Controller {
   static home(req, res) {
-    res.status(200).json({ Home: `Check this out` });
+    res.status(200).json({ Home: `Vercel Config Changed` });
   }
 
   static check(req, res) {
